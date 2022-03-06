189b48d957c79c0670a91fd4e983414a22fecad7
EC2 Default User <ec2-user@ip-172-31-93-125.ec2.internal>
2022-03-06 09:10:42 +0000

    [Update]index.html layout finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,16 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(list_params)
+    if @book.save
+      redirect_to index_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def show
@@ -7,4 +18,14 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  
+   # privateで定義するとURLとアクションが紐づかなくなる(privateメソッドの命名は「モデル名_params」とすることが多い)
+  private
+  def list_params
+    params.require(:book).permit(:title, :body)
+  end
+  # :ic
+  
+  
 end
