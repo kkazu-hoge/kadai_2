81dda584cb38a7aee8733a68d710cf36ecb45736
EC2 Default User <ec2-user@ip-172-31-93-125.ec2.internal>
2022-03-07 01:28:45 +0000

    [Finish]3-12

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   # get 'books' => 'books#index', as: 'index'
   # post 'books' => 'books#create', as: 'index_c'
   # delete 'books/:id' => 'books#destroy', as: 'index_d'
-
   # get 'books/:id' => 'books#show', as: 'show'
-
   # get 'books/:id/edit' => 'books#edit', as: 'edit'
   # patch 'books/:id' => 'books#update', as: 'edit_u'
 
